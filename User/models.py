806e11b78d83158c0204444b57b3dd37257c22dc
k89jy <k89jy@naver.com>
2022-03-18 15:31:00 +0900

    create test model user

---
+++ User/models.py
@@ -0,0 +1,21 @@
+from pyexpat import model
+from statistics import mode
+from django.db import models
+
+
+
+class User(models.Model):
+
+
+    
+user_email = models.CharField(max_length=100)
+password = models.CharField(max_length=100)
+
+confirm_password = models.CharField(max_length=100)
+
+nationality = models.CharField(max_length=100)
+
+join_date = models.CharField(max_length=100)
+
+leave_date = models.CharField(max_length=100)
+
